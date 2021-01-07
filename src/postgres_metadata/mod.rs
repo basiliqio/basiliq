d96584e0f17b2dd3f62efe6ecaaddebbe4996568
Francis Le Roy <francis.leroy@protonmail.ch>
2021-01-07 16:41:06 +0100

    Refactor to use itertool and hashmap for building the BasiliqTable object in a less naive manner

--- src/postgres_metadata/mod.rs
+++ src/postgres_metadata/mod.rs
@@ -2,3 +2,4 @@ use std::collections::HashMap;
 pub mod parsed;
 pub mod raw;
 use anyhow::{anyhow, bail, Result};
+use itertools::Itertools;
--- src/postgres_metadata/parsed.rs
+++ src/postgres_metadata/parsed.rs
@@ -10,7 +10,6 @@ pub struct BasiliqTable {
     pkeys: Vec<raw::PostgresPrimaryKeyRaw>,
     fkeys_child: Vec<raw::PostgresForeignKeyRaw>,
     fkeys_parent: Vec<raw::PostgresForeignKeyRaw>,
-    columns_store: Vec<Rc<BasiliqColumns>>,
     columns_by_name: HashMap<String, Rc<BasiliqColumns>>,
     columns_by_id: HashMap<i16, Rc<BasiliqColumns>>,
 }
@@ -37,103 +36,85 @@ impl BasiliqTable {
         primary_keys: Vec<raw::PostgresPrimaryKeyRaw>,
         foreign_keys: Vec<raw::PostgresForeignKeyRaw>,
     ) -> Result<Vec<Self>> {
-        let parsed_columns: Vec<BasiliqColumns> = BasiliqColumns::new(&types, &columns)?;
+        let types_map: HashMap<u32, raw::PostgresTypeRaw> =
+            types.into_iter().map(|x| (x.id(), x)).collect();
+        let columns_grouped: HashMap<u32, Vec<raw::PostgresColumnRaw>> = columns
+            .into_iter()
+            .map(|x| (x.table(), x))
+            .into_group_map_by(|x| x.0)
+            .into_iter()
+            .map(|(key, vals)| (key, vals.into_iter().map(|(_key, vals)| vals).collect()))
+            .collect();
+        let parsed_columns: HashMap<u32, Vec<BasiliqColumns>> =
+            BasiliqColumns::new(&types_map, columns_grouped)?;
         let mut res: Vec<BasiliqTable> = Vec::with_capacity(tables.len());
-
-        for table in tables.iter() {
-            let schema_for_table: raw::PostgresSchemaRaw =
-                BasiliqTable::find_schema_for_table(&schemas, &tables).ok_or(anyhow!(
-                    "The table {} doesn't belong to any schema",
-                    table.name()
-                ))?;
-            let columns_store: Vec<Rc<BasiliqColumns>> =
-                BasiliqTable::find_columns_for_table(&table, &parsed_columns)?;
+        let schemas_map: HashMap<u32, raw::PostgresSchemaRaw> =
+            schemas.into_iter().map(|x| (x.id(), x)).collect();
+        let primary_keys_map: HashMap<u32, Vec<raw::PostgresPrimaryKeyRaw>> = primary_keys
+            .into_iter()
+            .map(|x| (x.table(), x))
+            .into_group_map_by(|x| x.0)
+            .into_iter()
+            .map(|(key, vals)| (key, vals.into_iter().map(|(_key, vals)| vals).collect()))
+            .collect();
+        let foreign_keys_map_child: HashMap<u32, Vec<raw::PostgresForeignKeyRaw>> = foreign_keys
+            .clone()
+            .into_iter()
+            .map(|x| (x.table(), x))
+            .into_group_map_by(|x| x.0)
+            .into_iter()
+            .map(|(key, vals)| (key, vals.into_iter().map(|(_key, vals)| vals).collect()))
+            .collect();
+        let foreign_keys_map_parent: HashMap<u32, Vec<raw::PostgresForeignKeyRaw>> = foreign_keys
+            .into_iter()
+            .map(|x| (x.ftable(), x))
+            .into_group_map_by(|x| x.0)
+            .into_iter()
+            .map(|(key, vals)| (key, vals.into_iter().map(|(_key, vals)| vals).collect()))
+            .collect();
+        for table in tables.into_iter() {
+            let columns_store: Vec<Rc<BasiliqColumns>> = parsed_columns
+                .get(&table.id())
+                .ok_or(anyhow!("No columns defined for the table {}", table.name()))?
+                .clone()
+                .into_iter()
+                .map(|x| Rc::new(x))
+                .collect();
             let columns_by_name: HashMap<String, Rc<BasiliqColumns>> = columns_store
-                .iter()
-                .map(|x| (x.column().name().clone(), x.clone()))
+                .clone()
+                .into_iter()
+                .map(|x| (x.column().name().clone(), x))
                 .collect();
             let columns_by_id: HashMap<i16, Rc<BasiliqColumns>> = columns_store
-                .iter()
-                .map(|x| (x.column().column_number(), x.clone()))
+                .clone()
+                .into_iter()
+                .map(|x| (x.column().column_number(), x))
                 .collect();
-            let pkeys: Vec<raw::PostgresPrimaryKeyRaw> =
-                BasiliqTable::find_primary_keys_for_table(&table, &primary_keys);
-            let (fkeys_parent, fkeys_child): (
-                Vec<raw::PostgresForeignKeyRaw>,
-                Vec<raw::PostgresForeignKeyRaw>,
-            ) = BasiliqTable::find_foreign_keys_for_table(&table, &foreign_keys);
             res.push(BasiliqTable {
-                table: table.clone(),
-                columns_store,
+                schema: schemas_map
+                    .get(&table.schema())
+                    .ok_or(anyhow!(
+                        "The table {} is defined in an unknown schema : {}",
+                        table.name(),
+                        table.schema()
+                    ))?
+                    .clone(),
                 columns_by_name,
                 columns_by_id,
-                pkeys,
-                fkeys_parent,
-                fkeys_child,
-                schema: schema_for_table,
-            });
-        }
-        Ok(res)
-    }
-
-    fn find_primary_keys_for_table(
-        table: &raw::PostgresTableRaw,
-        primary_keys: &Vec<raw::PostgresPrimaryKeyRaw>,
-    ) -> Vec<raw::PostgresPrimaryKeyRaw> {
-        let mut res: Vec<raw::PostgresPrimaryKeyRaw> = Vec::with_capacity(1);
-
-        for pk in primary_keys.iter() {
-            if pk.table() == table.id() {
-                res.push(pk.clone());
-            }
-        }
-        res
-    }
-
-    fn find_foreign_keys_for_table(
-        table: &raw::PostgresTableRaw,
-        foreign_keys: &Vec<raw::PostgresForeignKeyRaw>,
-    ) -> (
-        Vec<raw::PostgresForeignKeyRaw>,
-        Vec<raw::PostgresForeignKeyRaw>,
-    ) {
-        let mut res_parent: Vec<raw::PostgresForeignKeyRaw> = Vec::new();
-        let mut res_child: Vec<raw::PostgresForeignKeyRaw> = Vec::new();
-
-        for fk in foreign_keys.iter() {
-            if fk.table() == table.id() {
-                res_child.push(fk.clone());
-            } else if fk.ftable() == table.id() {
-                res_parent.push(fk.clone());
-            }
-        }
-        (res_parent, res_child)
-    }
-
-    fn find_schema_for_table(
-        schemas: &Vec<raw::PostgresSchemaRaw>,
-        tables: &Vec<raw::PostgresTableRaw>,
-    ) -> Option<raw::PostgresSchemaRaw> {
-        for table in tables.iter() {
-            for schema in schemas.iter() {
-                if schema.id() == table.schema() {
-                    return Some(schema.clone());
-                }
-            }
-        }
-        None
-    }
-
-    fn find_columns_for_table(
-        table: &raw::PostgresTableRaw,
-        columns: &Vec<BasiliqColumns>,
-    ) -> Result<Vec<Rc<BasiliqColumns>>> {
-        let mut res: Vec<Rc<BasiliqColumns>> = Vec::with_capacity(columns.len());
-
-        for obj in columns.iter() {
-            if obj.column().table() == table.id() {
-                res.push(Rc::new(obj.clone()));
-            }
+                pkeys: primary_keys_map
+                    .get(&table.id())
+                    .cloned()
+                    .unwrap_or_default(),
+                fkeys_child: foreign_keys_map_child
+                    .get(&table.id())
+                    .cloned()
+                    .unwrap_or_default(),
+                fkeys_parent: foreign_keys_map_parent
+                    .get(&table.id())
+                    .cloned()
+                    .unwrap_or_default(),
+                table,
+            })
         }
         Ok(res)
     }
@@ -142,19 +123,19 @@ impl BasiliqTable {
 impl BasiliqType {
     pub fn new(
         type_: raw::PostgresTypeRaw,
-        list_available: &Vec<raw::PostgresTypeRaw>,
+        list_available: &HashMap<u32, raw::PostgresTypeRaw>,
     ) -> Result<Self> {
         match type_.child_type() {
             Some(child_id) => {
-                for available_type in list_available.iter() {
-                    if available_type.id() == child_id {
-                        return Ok(BasiliqType::Nested(
-                            type_.clone(),
-                            Box::new(BasiliqType::new(type_, list_available)?),
-                        ));
-                    }
-                }
-                bail!("Nested type {} not found for type {}", child_id, type_.id());
+                let child = list_available.get(&child_id).ok_or(anyhow!(
+                    "Nested type {} not found for type {}",
+                    child_id,
+                    type_.id()
+                ))?;
+                return Ok(BasiliqType::Nested(
+                    type_.clone(),
+                    Box::new(BasiliqType::new(child.clone(), list_available)?),
+                ));
             }
             None => Ok(BasiliqType::Simple(type_)),
         }
@@ -163,35 +144,28 @@ impl BasiliqType {
 
 impl BasiliqColumns {
     pub fn new(
-        types: &Vec<raw::PostgresTypeRaw>,
-        columns: &Vec<raw::PostgresColumnRaw>,
-    ) -> Result<Vec<Self>> {
-        Ok(BasiliqColumns::find_types_for_columns(types, columns)?
-            .into_iter()
-            .map(|(col, typ)| BasiliqColumns {
-                column: col,
-                type_: typ,
-            })
-            .collect())
-    }
-
-    fn find_types_for_columns(
-        types: &Vec<raw::PostgresTypeRaw>,
-        columns: &Vec<raw::PostgresColumnRaw>,
-    ) -> Result<Vec<(raw::PostgresColumnRaw, BasiliqType)>> {
-        let mut res: Vec<(raw::PostgresColumnRaw, BasiliqType)> = Vec::with_capacity(columns.len());
-
-        for column in columns.iter() {
-            for type_ in types.iter() {
-                match type_.rel_id() {
-                    Some(id) => {
-                        if id == column.table() {
-                            res.push((column.clone(), BasiliqType::new(type_.clone(), &types)?));
-                        }
-                    }
-                    None => continue,
-                }
+        types: &HashMap<u32, raw::PostgresTypeRaw>,
+        columns: HashMap<u32, Vec<raw::PostgresColumnRaw>>,
+    ) -> Result<HashMap<u32, Vec<Self>>> {
+        let mut res: HashMap<u32, Vec<Self>> = HashMap::with_capacity(columns.len());
+        for (key, cols) in columns.into_iter() {
+            let mut col_list: Vec<Self> = Vec::with_capacity(cols.len());
+            for col in cols.into_iter() {
+                let type_: raw::PostgresTypeRaw = types
+                    .get(&col.type_())
+                    .ok_or(anyhow!(
+                        "The type {} of column {} from table {} doesn't exists",
+                        col.type_(),
+                        col.name(),
+                        col.table()
+                    ))
+                    .map(|x| x.clone())?;
+                col_list.push(BasiliqColumns {
+                    column: col,
+                    type_: BasiliqType::new(type_, types)?,
+                });
             }
+            res.insert(key, col_list);
         }
         Ok(res)
     }
