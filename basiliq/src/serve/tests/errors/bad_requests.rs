6cbfbf7c5aeaf02a0450c4174785e74d15dcfa0c
Francis Le Roy <francis.leroy@protonmail.ch>
2021-04-15 16:17:25 +0200

    Added base tests for basiliq*

---
+++ basiliq/src/serve/tests/errors/bad_requests.rs
@@ -0,0 +1,68 @@
+use super::*;
+
+#[basiliq_test(run_migrations)]
+async fn unknown_resource(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(Method::GET, "/an_unknown_resource", Body::empty());
+
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteUnknownType).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn bad_content_type(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let mut request = prepare_basiliq_request(Method::GET, "/an_unknown_resource", Body::empty());
+    request.headers_mut().insert(
+        header::CONTENT_TYPE,
+        HeaderValue::from_static("application/json"),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::UNSUPPORTED_MEDIA_TYPE);
+    handle_errors(resp, BasiliqErrorId::BadContentType).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn bad_method(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(Method::PUT, "/public__peoples", Body::empty());
+
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::METHOD_NOT_ALLOWED);
+    handle_errors(resp, BasiliqErrorId::BadMethod).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn bad_json(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::POST,
+        "/public__peoples",
+        Body::from("AAAAAAAAAAAAAAA"),
+    );
+
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::BadJson).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn bad_uuid(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(Method::GET, "/public__peoples/AAAAAAAA", Body::empty());
+
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::BadUuid).await;
+}
---
+++ basiliq/src/serve/tests/errors/mod.rs
@@ -0,0 +1,19 @@
+use super::*;
+use crate::serve::errors::BasiliqErrorId;
+use ciboulette::CibouletteErrorRequest;
+mod bad_requests;
+use hyper::header::{self, HeaderValue};
+
+pub async fn handle_errors<'a>(
+    response: Response<Body>,
+    expected_code: BasiliqErrorId,
+) -> ciboulette::CibouletteErrorObj<'a> {
+    let bytes = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    let obj: CibouletteErrorRequest = serde_json::from_slice(&bytes).unwrap();
+    assert_eq!(obj.errors().id().as_ref().unwrap(), expected_code.id());
+    assert_eq!(
+        obj.errors().title().as_ref().unwrap(),
+        expected_code.title()
+    );
+    obj.errors
+}
