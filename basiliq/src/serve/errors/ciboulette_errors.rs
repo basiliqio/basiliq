70c3c41a2d2853c58f3e62c85f5a508f794a324a
Francis Le Roy <francis.leroy@protonmail.ch>
2021-04-16 12:08:26 +0200

    Added more tests for ciboulette induced errors

--- basiliq/src/serve/errors/ciboulette_errors.rs
+++ basiliq/src/serve/errors/ciboulette_errors.rs
@@ -37,78 +37,28 @@ pub fn handle_ciboulette_error<'a>(
         CibouletteError::UnknownField(_, _) => {
             cib_err!(StatusCode::BAD_REQUEST, CibouletteUnknownField, err)
         }
-        CibouletteError::IncompatibleSorting => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteIncompatibleSorting, err)
-        }
         CibouletteError::NestedSorting => {
             cib_err!(StatusCode::BAD_REQUEST, CibouletteNestedSorting, err)
         }
-        CibouletteError::UniqObj(_, _) => cib_err!(StatusCode::BAD_REQUEST, CibouletteUniqObj, err),
-        CibouletteError::UniqType(_) => cib_err!(StatusCode::BAD_REQUEST, CibouletteUniqType, err),
-        CibouletteError::UniqRelationshipObject(_, _) => cib_err!(
-            StatusCode::BAD_REQUEST,
-            CibouletteUniqRelationshipObject,
-            err
-        ),
-        CibouletteError::UniqRelationship(_, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteUniqRelationship, err)
-        }
-        CibouletteError::MissingLink(_, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteMissingLink, err)
-        }
-        CibouletteError::NoCompleteLinkage(_, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteNoCompleteLinkage, err)
-        }
-        CibouletteError::TypeNotInGraph(_) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteTypeNotInGraph, err)
-        }
-        CibouletteError::RelNotInGraph(_, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteRelNotInGraph, err)
-        }
-        CibouletteError::KeyClash(_, _, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteKeyClash, err)
-        }
-        CibouletteError::InvalidMemberName(_) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteInvalidMemberName, err)
-        }
-        CibouletteError::AttributesIsNotAnObject => cib_err!(
-            StatusCode::BAD_REQUEST,
-            CibouletteAttributesIsNotAnObject,
-            err
-        ),
-        CibouletteError::EmptyQueryAttribute => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteEmptyQueryAttribute, err)
-        }
         CibouletteError::NoData => cib_err!(StatusCode::BAD_REQUEST, CibouletteNoData, err),
         CibouletteError::MissingId => cib_err!(StatusCode::BAD_REQUEST, CibouletteMissingId, err),
-        CibouletteError::BadIdType(_, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteBadIdType, err)
-        }
-        CibouletteError::UnknownIdType(_) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteUnknownIdType, err)
-        }
         CibouletteError::NoCompound => cib_err!(StatusCode::BAD_REQUEST, CibouletteNoCompound, err),
-        CibouletteError::MissingAliasTranslation(_, _) => cib_err!(
-            StatusCode::BAD_REQUEST,
-            CibouletteMissingAliasTranslation,
-            err
-        ),
         CibouletteError::MissingTypeInPath => {
             cib_err!(StatusCode::BAD_REQUEST, CibouletteMissingTypeInPath, err)
         }
         CibouletteError::BadPath => cib_err!(StatusCode::BAD_REQUEST, CibouletteBadPath, err),
-        CibouletteError::WrongIntention(_, _) => {
-            cib_err!(StatusCode::BAD_REQUEST, CibouletteWrongIntention, err)
-        }
         CibouletteError::WrongPathType(_, _) => {
             cib_err!(StatusCode::BAD_REQUEST, CibouletteWrongPathType, err)
         }
-        CibouletteError::OutboundTooManyMainData(_) => cib_err!(
-            StatusCode::BAD_REQUEST,
-            CibouletteOutboundTooManyMainData,
-            err
-        ),
         CibouletteError::UnknownError(_) => {
+            cib_err!(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                CibouletteUnknownError,
+                err
+            )
+        }
+        _ => {
+            // FIXME Add a way to get more insight
             cib_err!(
                 StatusCode::INTERNAL_SERVER_ERROR,
                 CibouletteUnknownError,
--- basiliq/src/serve/tests/errors/ciboulette_errors/format.rs
+++ basiliq/src/serve/tests/errors/ciboulette_errors/format.rs
@@ -52,3 +52,221 @@ async fn unknown_relationships(pool: sqlx::PgPool) {
     assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
     handle_errors(resp, BasiliqErrorId::CibouletteUnknownRelationship).await;
 }
+
+#[basiliq_test(run_migrations)]
+async fn unknown_fields(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::GET,
+        "/public__peoples?fields[public__peoples]=AAAAAA",
+        Body::empty(),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteUnknownField).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn nested_sorting(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::GET,
+        "/public__peoples?sort=articles.comments.body",
+        Body::empty(),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteNestedSorting).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn no_data(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::POST,
+        "/public__peoples",
+        Body::from(
+            json!({
+                "meta": json!({
+                    "something": "HAHAH",
+                })
+            })
+            .to_string(),
+        ),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteNoData).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn missing_id(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::PATCH,
+        "/public__peoples/f781f789-e701-4edc-a66c-8f7e19036b7c",
+        Body::from(
+            json!({
+                "data": json!({
+                    "type": "public__peoples",
+                    "attributes": json!({
+                        "first-name": "Francis"
+                    })
+                })
+            })
+            .to_string(),
+        ),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteMissingId).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn bad_id_type(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::POST,
+        "/public__peoples",
+        Body::from(
+            json!({
+                "data": json!([{
+                    "type": "public__peoples",
+                    "id": "0db3c22c-d20d-4c23-9b01-0f0cc1e465f7",
+                    "attributes": json!({
+                        "first-name": "Francis",
+                        "last-name": "it's me",
+                    })
+                },
+                {
+                    "type": "public__peoples",
+                    "id": "fde25783-ff24-49e3-ba2b-00b5697222df",
+                    "attributes": json!({
+                        "first-name": "Francis",
+                        "last-name": "it's not me",
+                    })
+                },
+                ])
+            })
+            .to_string(),
+        ),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteNoCompound).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn no_compound(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::POST,
+        "/public__peoples",
+        Body::from(
+            json!({
+                "data": json!([{
+                    "type": "public__peoples",
+                    "id": "0db3c22c-d20d-4c23-9b01-0f0cc1e465f7",
+                    "attributes": json!({
+                        "first-name": "Francis",
+                        "last-name": "it's me",
+                    })
+                },
+                {
+                    "type": "public__peoples",
+                    "id": "fde25783-ff24-49e3-ba2b-00b5697222df",
+                    "attributes": json!({
+                        "first-name": "Francis",
+                        "last-name": "it's not me",
+                    })
+                },
+                ])
+            })
+            .to_string(),
+        ),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteNoCompound).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn missing_type_in_path(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::POST,
+        "/", // this
+        Body::from(
+            json!({
+                "data": json!({
+                    "type": "public__peoples",
+                    "id": "0db3c22c-d20d-4c23-9b01-0f0cc1e465f7",
+                    "attributes": json!({
+                        "first-name": "Francis",
+                        "last-name": "it's me",
+                    })
+                })
+            })
+            .to_string(),
+        ),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteMissingTypeInPath).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn bad_path(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::GET,
+        "/public__peoples/0db3c22c-d20d-4c23-9b01-0f0cc1e465f7/cantevenspellrelationships/public__articles", // this
+        Body::empty(),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteBadPath).await;
+}
+
+#[basiliq_test(run_migrations)]
+async fn wrong_path_type(pool: sqlx::PgPool) {
+    let state = prepare_basiliq_test(pool).await;
+    let request = prepare_basiliq_request(
+        Method::POST,
+        "/public__peoples/0db3c22c-d20d-4c23-9b01-0f0cc1e465f7", // this
+        Body::from(
+            json!({
+                "data": json!({
+                    "type": "public__peoples",
+                    "id": "0db3c22c-d20d-4c23-9b01-0f0cc1e465f7",
+                    "attributes": json!({
+                        "first-name": "Francis",
+                        "last-name": "it's me",
+                    })
+                })
+            })
+            .to_string(),
+        ),
+    );
+    let resp = crate::serve::main_service(state.clone(), request)
+        .await
+        .unwrap();
+    assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    handle_errors(resp, BasiliqErrorId::CibouletteWrongPathType).await;
+}
