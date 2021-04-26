0d884486c7fff3b9a29d021e666348b5abb68d49
Francis Le Roy <francis.leroy@protonmail.ch>
2021-04-26 11:31:27 +0200

    Finish creating tests

--- basiliq/src/serve/tests/requests/create/mod.rs
+++ basiliq/src/serve/tests/requests/create/mod.rs
@@ -2,6 +2,7 @@ use super::*;
 mod include;
 mod misc;
 mod normal;
+mod paths;
 mod relationships;
 mod sparse;
 
--- basiliq/src/serve/tests/run_test.rs
+++ basiliq/src/serve/tests/run_test.rs
@@ -3,13 +3,13 @@ use super::*;
 pub async fn run_request(
     state: Arc<BasiliqServerState>,
     method: hyper::Method,
-    query: &str,
+    query: String,
     expected_status: u16,
     body: Option<serde_json::Value>,
 ) -> serde_json::Value {
     let request = prepare_basiliq_request(
         method,
-        query,
+        query.as_str(),
         match body {
             Some(x) => Body::from(x.to_string()),
             None => Body::empty(),
@@ -29,22 +29,22 @@ pub async fn run_request(
 
 #[macro_export]
 macro_rules! run_test_request {
-    ($name:ident, $method:expr, $query:literal, $expected_status:literal) => {
+    ($name:ident, $method:expr, $query:expr, $expected_status:literal) => {
         #[basiliq_test(run_migrations, init_values)]
         async fn $name(pool: sqlx::PgPool) {
             let state = prepare_basiliq_test(pool).await;
 
-            let res = crate::serve::tests::run_request(state, $method, $query, $expected_status, None).await;
+            let res = crate::serve::tests::run_request(state, $method, $query.into(), $expected_status, None).await;
 			insta::assert_json_snapshot!(res);
         }
     };
 
-	($name:ident, $method:expr, $query:literal, $expected_status:literal, $body:expr) => {
+	($name:ident, $method:expr, $query:expr, $expected_status:literal, $body:expr) => {
 		#[basiliq_test(run_migrations, init_values)]
 		async fn $name(pool: sqlx::PgPool) {
 			let state = prepare_basiliq_test(pool).await;
 
-			let res = crate::serve::tests::run_request(state, $method, $query, $expected_status, Some($body)).await;
+			let res = crate::serve::tests::run_request(state, $method, $query.into(), $expected_status, Some($body)).await;
 			insta::assert_json_snapshot!(res,
 			{
 				".**.id" => insta::dynamic_redaction(crate::serve::tests::check_uuid),
