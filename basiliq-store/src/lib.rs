009fbe9a335944e88988ff5cd00ac928849ca73b
Francis Le Roy <francis.leroy@protonmail.ch>
2021-05-02 13:41:42 +0200

    Pushing docker image to docker hub too

--- basiliq-store/src/lib.rs
+++ basiliq-store/src/lib.rs
@@ -1,3 +1,13 @@
+//! # Introduction
+//!
+//! The **_BasiliqStore_** crate is a helper crate for the [**_Basiliq_**](https://github.com/basiliqio/basiliq)
+//! project.
+//!
+//! It allows to build the [BasiliqStore](BasiliqStore) necessary to run the [**_Basiliq_**](https://github.com/basiliqio/basiliq)
+//! server.
+//!
+//! The [BasiliqStore](BasiliqStore) is built by scanning the database for metadatas and then merging configuration
+//! provided by the user.
 mod postgres_metadata;
 mod store;
 use arcstr::ArcStr;
